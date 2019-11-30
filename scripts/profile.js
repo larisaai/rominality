4780914392e770c6d171d9bf00568bb8ee3e0d1f
Larisa <ailisoaielarisa@yahoo.com>
2019-11-30 17:42:42 +0100

    login/signup system everything working

---
+++ scripts/profile.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".delete-btn").addEventListener("click", function(){
+    document.querySelector(".delete-account-container").style.display = "block";
+  });
+
+  document.querySelector(".cancel-btn").addEventListener("click", function(){
+    document.querySelector(".delete-account-container").style.display = "none";
+  });
