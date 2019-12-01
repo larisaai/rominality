89e7265b1430bed7efd39fd484974d6b37a576eb
Larisa <ailisoaielarisa@yahoo.com>
2019-12-01 22:27:47 +0100

    profile page work in progress

--- includes/menu_logged.php
+++ includes/menu_logged.php
@@ -1,15 +1,22 @@
 <?php
 
-echo ' <nav class="navbar navbar-default" role="navigation">
-<div class="container-fluid">
+echo '
+<nav class="navbar  " >
+        <div class="box-wide">
+            <div class="navigation ">
+                    <div class="nav-left-aligned"><h4>rominality</h4></div>
+                    <div class="nav-right-aligned">
+                        <li><a href="../users/latest_releases.php">Home</a></li>
+                        <li><a href="../users/library.php">Library</a></li>
+                        <li><a href="../users/profile.php">Profile</a></li>
+                        <li><a href="../users/logout.php">Logout</a></li>
+                        <div class="nav-two-design-lines">
+                            <div class="nav-design-line"></div>
+                            <div class="nav-design-line"></div>
+                        </div>
+                    </div>
+            </div>
+        </div>
 
-    <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li><a href="../users/latest_releases.php">Home</a></li>
-            <li><a href="../users/library.php">Library</a></li>
-            <li><a href="../users/profile.php">Profile</a></li>
-            <li><a href="../users/logout.php">Logout</a></li>
-        </ul>
-    </div>
-</div>
-</nav>';
+    </nav>
+';
--- users/profile.php
+++ users/profile.php
@@ -32,74 +32,75 @@
     require_once('../includes/menu_logged.php');
     ?>
     <div class="container">
+        <div class="hero-img-container">
+            <img class="hero-img" src="../img/hero2.png">
+        </div>
 
-
-        <div class="row top-buffer">
-            <h3>Your profile</h3>
-        
-            <div class="editable-data">
-            <div class="row top-buffer">
-            <div class="col-xs-8 col-xs-offset-2">
-                <form class="form-horizontal" method="POST" action="profile.php" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="title" class="col-sm-2 control-label">First Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="title" placeholder="First Name" name="firstname" value="<?php echo $_SESSION['user']['firstname'];  ?>" required>
+        <div class="box-wide">
+            <div class="profile-page-inner">
+                <h3>My profile</h3>
+            
+                <div class="editable-data">
+                <div >
+                <div class="">
+                    <form class="form-horizontal" method="POST" action="profile.php" enctype="multipart/form-data">
+                        <div class="form-group">
+                            
+                            <div class="">
+                                <input type="text" class="form-control" placeholder="First Name" name="firstname" value="<?php echo $_SESSION['user']['firstname'];  ?>" required>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="year" class="col-sm-2 control-label">Last Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="year" placeholder="Last Name" name="lastname" value="<?php echo $_SESSION['user']['lastname']; ?>" required>
-                        </div>  
-                    </div>
-                    <div class="form-group">
-                        <label for="director" class="col-sm-2 control-label">Email</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="director" placeholder="email" name="email" value=" <?php echo $_SESSION['user']['email']; ?>" required>
+                        <div class="form-group">
+                            <div class="">
+                                <input type="text" class="form-control"  placeholder="Last Name" name="lastname" value="<?php echo $_SESSION['user']['lastname']; ?>" required>
+                            </div>  
                         </div>
-                    </div>
-
-                
-                    <div class="form-group">
-                        <label for="director" class="col-sm-2 control-label">Add a picture</label>
-                        <div class="col-sm-10">
-                            <input type="file" class="form-control" name="profileImage" accept="image/png, image/jpeg" >
+                        <div class="form-group">
+                            <div class="">
+                                <input type="email" class="form-control" placeholder="email" name="email" value=" <?php echo $_SESSION['user']['email']; ?>" required>
+                            </div>
                         </div>
-                    </div>
+
+                    
+                        <div class="form-group">
+                            <div class="">
+                                <input type="file" class="form-control" name="profileImage" accept="image/png, image/jpeg" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <a href="change_password.php" class="change-pass"> Change password </a>
+                        </div>
+                    
+                    
+
+                        <div class="form-group">
+                            <div class="">
+                                <input type="submit" class="button btn-white " value="Update profile">
+                            </div>
+                        </div>
+                    </form>
 
                     <div class="form-group">
-                        <a href="change_password.php" class="col-sm-offset-2 col-sm-10"> Change password </a>
-                    </div>
-                  
-                   
-
-                    <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                            <input type="submit" class="btn btn-primary" value="Update profile">
+                            <div class="">
+                            <button class="button btn-white delete-btn">Delete profile</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
 
-                <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                          <button class="delete-btn">Delete profile</button>
+                    <div class="image-container">
+                        <img src="<?php echo $_SESSION['user']['profile_picture']; ?>" alt="">    
+                    </div>
+
+                    <div class="delete-account-container">
+                        <div class="delete-account-content">
+                            <p>Are you sure that you want to delete your account?</p> 
+                            <button class="button btn-white  cancel-btn">Cancel</button>
+                            <a href="delete_user.php"><button class="button btn-white  confirm-delete-btn">Yes</button></a>
                         </div>
-                    </div>
-
-                <div class="image-container">
-                    <img src="<?php echo $_SESSION['user']['profile_picture']; ?>" alt="">    
-                </div>
-
-                <div class="delete-account-container">
-                    <div class="delete-account-content">
-                        <p>Are you sure that you want to delete your account?</p> 
-                        <button class="cancel-btn">Cancel</button>
-                        <a href="delete_user.php"><button class="confirm-delete-btn">Yes</button></a>
                     </div>
                 </div>
             </div>
-        </div>
+                </div>
             </div>
         </div>
     </div>
