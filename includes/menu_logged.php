a9da91c5e7abd8825d06cd363a130c3f63ba3d12
Larisa <ailisoaielarisa@yahoo.com>
2019-12-07 15:17:21 +0100

    fixing some style

--- includes/menu_logged.php
+++ includes/menu_logged.php
@@ -16,7 +16,7 @@ echo '
                         <li><a href="../users/latest_releases.php">Home</a></li>
                         <li><a href="../users/library.php">Library</a></li>
                         
-                        <div class="sub-menu-header"><a >';
+                        <div class="sub-menu-header"><a class="user-name">';
 echo $_SESSION['user']['firstname']; ?>
                          <?php 
                             echo '
@@ -24,7 +24,7 @@ echo $_SESSION['user']['firstname']; ?>
                             <div class="sub-menu"></a>
                        
                                 <li> <a href="../users/profile.php">My profile</a></li>
-                                <li> <a href="../users/cart.php">My cart <span id="cartItems">' . $cartItems . '</span></a></li>
+                                <li> <a href="../users/cart.php">My cart ( <span id="cartItems">' . $cartItems . '</span> )</a></li>
                                 <li><a href="../users/logout.php">Logout</a></li>
                             </div>
                            
