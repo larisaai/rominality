66a782af58c6e08b37729551db5521816ab331b6
Larisa <ailisoaielarisa@yahoo.com>
2019-12-08 11:03:57 +0100

    burger menu fixed

--- includes/menu_logged.php
+++ includes/menu_logged.php
@@ -11,8 +11,16 @@ echo '
 <nav class="navbar  " >
         <div class="box-wide">
             <div class="navigation ">
-                    <div class="nav-left-aligned"><h4>rominality</h4></div>
+                <div class="nav-left-aligned"><h4>rominality</h4></div>
+                <div class="burger-menu-container">
+                    <div class="burger-menu">
+                        <span class="burger-menu-item"></span>
+                        <span class="burger-menu-item"></span>
+                        <span class="burger-menu-item"></span>
+                    </div>
+                </div>
                     <div class="nav-right-aligned">
+                   
                         <li><a href="../users/latest_releases.php">Home</a></li>
                         <li><a href="../users/library.php">Library</a></li>
                         
--- scripts/drop-down-sub-menu.js
+++ scripts/drop-down-sub-menu.js
@@ -1,4 +1,10 @@
 
+//burger menu
+document.querySelector(".burger-menu").addEventListener("click", function(){
+  document.querySelector(".navbar").classList.toggle("open");
+ 
+  
+});
 //drop down secondary menu
 document.querySelector(".sub-menu-header").addEventListener("click", function(){
   document.querySelector(".sub-menu-header").classList.toggle("active");
