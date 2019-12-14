02cea2ee30576933b3548f33157024bff95e92c5
bertea74@gmail.com <bertea74@gmail.com>
2019-12-14 13:46:08 +0200

    defined index.php

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/app.css" />
-    <title>Rominality</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="container-fluid">
-       
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="users/login.php">Login</a></li>
-                    <li><a href="users/signup.php">Signup</a></li>
-                 
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-       
-    </div>
-</body>
-
-</html>
+header("Location: users/login.php");
