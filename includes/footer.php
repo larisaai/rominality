ad68087da397605090c1bfeeb285b960ac8ab920
Larisa <ailisoaielarisa@yahoo.com>
2019-12-16 21:51:09 +0100

    work in progress

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@ echo '
 <div class="footer-container">
     <footer class="footer ">   
         <div class="copy-rights"><p>© copyrights reserved — 2020</p></div>
-        <div class="logo"><img style="width:150px" src="../img/logo2.png" /></div>
+        <div class="logo"><img style="width:150px" src="../img/logo-black.png" /></div>
         <div class="social-media">
             <div id="fb-share-button">
                 <img src="../img/facebook.png">
