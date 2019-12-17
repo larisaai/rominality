97b5324dcead833a5518871fd45eb91babd22a17
Stefan Andrei <stefandrei123@gmail.com>
2019-12-17 22:03:33 +0100

    added About Us

--- components/about-us-component.php
+++ components/about-us-component.php
@@ -1,14 +1,19 @@
 <div class="about-us-container">
-        <div class="box-wide about-us-inner-container">
-            <div class="about-us-top-left-short-line"></div>
-            <div class="about-us-top-right-short-line"></div>
-            <div class="about-us-bottom-left-short-line"></div>
-            <div class="about-us-bottom-right-short-line"></div>
-            <div class="about-us box-wide-inner">
-                <h2 class="about-title">About us</h2>
-                <p class="about-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
+    <div class="box-wide about-us-inner-container">
+        <div class="about-us-top-left-short-line"></div>
+        <div class="about-us-top-right-short-line"></div>
+        <div class="about-us-bottom-left-short-line"></div>
+        <div class="about-us-bottom-right-short-line"></div>
+        <div class="about-us box-wide-inner">
+            <h2 class="about-title">About us</h2>
+            <p class="about-description"><b>Welcome to Rominality!</b><br> Here you can play, share and enjoy your favorite minimal music.<br><br>
+                Rominality is a web service platform dedicated to rominimal music lovers.
 
+                You can sign up as a user or log in into your account and enjoy the posted songs by our favorite producers. We encourage you to engage with others and to support your favorite artist by buying their song directly on our platform.<br><br>
+
+                <b>Play, share and enjoy music.</b></p>
         </div>
 
     </div>
+
+</div>
